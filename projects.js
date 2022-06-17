394e95050623dd0844fd0d4fa4b8d37a2a7fb192
bas11111 <brouwer.bas@gmail.com>
2022-06-17 10:50:13 +0200

    portfolio

---
+++ projects.js
@@ -0,0 +1,22 @@
+let klik = document.querySelector('.project1')
+
+klik.addEventListener("click", function(){
+
+    window.open('https://bas11111.github.io/Irritand-website/')
+})
+
+
+let klikker = document.querySelector('.project2')
+
+klikker.addEventListener("click", function(){
+
+    window.open('https://bas11111.github.io/dino/')
+})
+
+
+let kikker = document.querySelector('.project3')
+
+kikker.addEventListener("click", function(){
+
+    window.open()
+})
